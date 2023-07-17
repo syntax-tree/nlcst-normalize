0cddcbc5c10cb5852c119253e7bd9f16d6d74a59
Titus Wormer <tituswormer@gmail.com>
2023-07-17 15:01:14 +0200

    Remove deprecated `NormalizeOptions` type

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 /**
  * @typedef {import('./lib/index.js').Options} Options
- * @typedef {import('./lib/index.js').Options} NormalizeOptions
- *   Deprecated form of `Options`.
- *
- *   To do: remove.
  */
 
 export {normalize} from './lib/index.js'
