37ede6934953aa8e506813dcb2f72ab9abb5de2b
Titus Wormer <tituswormer@gmail.com>
2023-07-17 14:54:26 +0200

    Update `@types/nlcst`, utilities

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * @typedef {import('./lib/index.js').Options} Options
  * @typedef {import('./lib/index.js').Options} NormalizeOptions
  *   Deprecated form of `Options`.
+ *
+ *   To do: remove.
  */
 
 export {normalize} from './lib/index.js'
